bcbbbb22ad70faa589c818189f31abf67418780d
unknown <vasunagpal@vasu368301.Nagarro.local>
2015-07-24 10:02:37 +0530

    first commit

---
+++ KODatatableServerSidePaging/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KODatatableServerSidePaging.Models;
+
+namespace KODatatableServerSidePaging.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Method to retrieve records from server
+        /// </summary>
+        /// <param name="GlobalSearchText">Global serach text</param>
+        /// <param name="RecordsToTake">No of records to be fetched</param>
+        /// <param name="RecordsToSkip">No of records to be skipped</param>
+        /// <param name="Columns">Gives you info about each column and if it is sortable or not</param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult GetCollection(GlobalSearchText GlobalSearchText, int RecordsToTake, int RecordsToSkip, List<DataTableConfig> Columns)
+        {
+            //Implement custom logic to fetch records
+            List<TableModel> objLs = new List<TableModel>();
+            for (int i = 0; i < 12; i++)
+            {
+                TableModel obj = new TableModel();
+                obj.Id = i;
+                obj.Address = "Address" + i;
+                obj.Name = "vasu" + i;
+                objLs.Add(obj);
+            }
+            return Json(objLs);
+        }
+        [HttpPost]
+        public JsonResult GetCollection1()
+        {
+            //Implement custom logic to fetch records
+            List<TableModel> objLs = new List<TableModel>();
+            for (int i = 0; i < 12; i++)
+            {
+                TableModel obj = new TableModel();
+                obj.Id = i;
+                obj.Address = "Address" + i;
+                obj.Name = "vasu" + i;
+                objLs.Add(obj);
+            }
+            return Json(objLs);
+        }
+
+    }
+
+   
+}
---
+++ KODatatableServerSidePaging/Models/DatatTablepagingModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KODatatableServerSidePaging.Models
+{
+    
+        public class TableModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+        }
+
+
+    //models required by data table
+    //this is used to send the search query text
+        public class GlobalSearchText
+        {
+            public string value { get; set; }
+            public string regex { get; set; }
+        }
+
+    //this model is used to send the information about each column
+        public class DataTableConfig
+        {
+            public string ColumnName { get; set; }
+            public bool IsSorted { get; set; }
+            public string SearchText { get; set; }
+            public bool IsSearchable { get; set; }
+            public string SortDirection { get; set; }
+
+        }
+    
+}
